505abd5ba6a10ed3d3bb80559674e0dec488e61a
신상용 <kurt-hide@sinsang-yong-ui-MacBookAir.local>
2025-04-19 02:10:58 +0900

    chore: 초기세팅 완료

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TodoQueryProviders from "@/providers/TodoQueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,15 +20,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <TodoQueryProviders>
+          {children}
+        </TodoQueryProviders>
       </body>
     </html>
   );
